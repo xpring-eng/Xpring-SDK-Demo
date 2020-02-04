bb7d8e06c7ef8168ca2fd5231c810046d3d9236e
Keefer Taylor <keefer.t@gmail.com>
2020-02-04 15:23:56 -0500

    swift demo

--- node/README.md
+++ node/README.md
@@ -1,6 +1,8 @@
 # Node Demo
 
-Demonstrates [Xpring-JS](http://github.com/xpring-eng/xpring-js). 
+Demonstrates [Xpring-JS](http://github.com/xpring-eng/xpring-js). This demo uses
+[Node JS](http://nodejs.org) and assumes node is installed and you have a working
+knowledge of using Node. 
 
 To build:
 ```shell
--- node/src/index.js
+++ node/src/index.js
@@ -1,4 +1,4 @@
-const { Wallet, XpringClient } = require("xpring-js")
+const { TransactionStatus, Wallet, XpringClient } = require("xpring-js")
 
 // The expected address of the gRPC server.
 const grpcURL = "3.14.64.116:50051";
@@ -11,23 +11,39 @@ const dropsToSend = "10";
 
 async function main() {
   console.log("\nUsing rippled node located at: " + grpcURL + "\n");
-  const xrpClient = new XpringClient(grpcURL, true);
+  const xpringClient = new XpringClient(grpcURL, true);
 
   console.log("Retrieving balance for " + wallet.getAddress() + "..");
-  const balance = await xrpClient.getBalance(wallet.getAddress());
+  const balance = await xpringClient.getBalance(wallet.getAddress());
   console.log("Balance was " + balance + " drops!\n");
 
   console.log("Sending:");
-  console.log("- Drops "+ dropsToSend)
+  console.log("- Drops: "+ dropsToSend)
   console.log("- To: " + recipientAddress);
   console.log("- From: " + wallet.getAddress());
-  const result = await xrpClient.send(
+  const hash = await xpringClient.send(
     dropsToSend,
     recipientAddress,
     wallet
   )
+  console.log("Hash for transaction:\n" + hash + "\n");
 
-  console.log("Hash for transaction:\n" + result);
+  const transactionStatus = await xpringClient.getTransactionStatus(hash);
+  console.log("Status of transaction is:\n")
+  console.log(transcationStatusToString(transactionStatus) + "\n");
+}
+
+function transcationStatusToString(transactionStatus) {
+  switch (transactionStatus) {
+    case TransactionStatus.Pending:
+      return "Pending"
+    case TransactionStatus.Failed:
+      return "Failed"
+    case TransactionStatus.Succeeded:
+      return "Succeeded"
+    default:
+      return "Unknown'"
+  }
 }
 
 main()
