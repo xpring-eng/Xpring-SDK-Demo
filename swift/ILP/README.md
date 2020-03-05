b75baa533352a83af9a33433aee905e5fc5ed1d9
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-05 18:53:52 -0500

    swift ilp demo

---
+++ swift/ILP/README.md
@@ -0,0 +1,20 @@
+# Swift Demo
+
+<img src="swift-demo.png" alt="Screenshot of the Xpring SDK Demo"/>
+
+Demonstrates [XpringKit](http://github.com/xpring-eng/xpringkit). 
+
+This demo builds using [Carthage](https://github.com/Carthage/Carthage) but it is also possible to build XpringKit with [CocoaPods](https://cocoapods.org/).
+
+To build:
+```shell
+# Install required tooling
+brew install carthage swiftlint
+
+# Install dependencies
+carthage bootsrap
+
+# Run in XCode.
+open XpringKit.xcodeproj
+```
+
---
+++ swift/ILP/Sources/main.swift
@@ -0,0 +1,33 @@
+import Foundation
+import XpringKit
+
+let grpcUrl = "hermes-grpc.ilpv4.dev"
+let demoUserId = "demo_user"
+let demoUserAuthToken = "2S1PZh3fEKnKg"
+
+print("\nUsing Hermes node located at: \(grpcUrl) \n")
+let ilpClient = IlpClient(grpcUrl: grpcUrl)
+
+
+print("Retrieving balance for \(demoUserId)...")
+let getBalance = try ilpClient.getBalance(for: demoUserId, bearerToken: demoUserAuthToken)
+print("Net balance was \(getBalance.netBalance) with asset scale \(getBalance.assetScale)")
+
+let receiverPaymentPointer = "$money.ilpv4.dev/demo_receiver"
+let amountToSend: UInt64 = 100
+print("\nSending payment:")
+print("- From: \(demoUserId)")
+print("- To: \(receiverPaymentPointer)")
+print("- Amount: \(amountToSend) drops")
+let payment = try ilpClient.sendPayment(amount: amountToSend,
+                                        paymentPointer: receiverPaymentPointer,
+                                        senderAccountId: demoUserId,
+                                        bearerToken: demoUserAuthToken)
+
+print("\nPayment sent!")
+print("Amount sent: \(payment.amountSent)")
+print("Amount delivered: \(payment.amountDelivered)")
+print("Payment was \((payment.successfulPayment ? "successful!" : "unsuccessful!"))")
+
+let balanceAfterPayment = try ilpClient.getBalance(for: demoUserId, bearerToken: demoUserAuthToken);
+print("Net balance after sending payment was \(balanceAfterPayment.netBalance)")
