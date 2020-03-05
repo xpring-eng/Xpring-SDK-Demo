539ced222b0ea3d0a8ee67400e2fa7627d649ea3
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-05 10:28:32 -0500

    rebase index.js to index-xrp.js rebase node-demo.png to node-xrp-demo.png add ilp screenshot update README.md at root and node/
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>

--- node/src/index-ilp.js
+++ node/src/index-ilp.js
@@ -3,6 +3,7 @@ const { IlpClient } = require("xpring-js")
 const grpcUrl = 'hermes-grpc.ilpv4.dev'
 const demoUserId = "demo_user"
 const demoUserAuthToken = "2S1PZh3fEKnKg"
+
 async function main() {
 
   console.log("\nUsing Hermes node located at: " + grpcUrl + "\n")
@@ -14,13 +15,13 @@ async function main() {
 
   const receiverPaymentPointer = "$money.ilpv4.dev/demo_receiver";
   let amountToSend = 100;
-  console.log("Sending payment:")
+  console.log("\nSending payment:")
   console.log("- From: " + demoUserId)
   console.log("- To: " + receiverPaymentPointer)
   console.log("- Amount: " + amountToSend + " drops")
   const payment = await ilpClient.send(amountToSend, receiverPaymentPointer, demoUserId, demoUserAuthToken);
 
-  console.log("Payment sent!")
+  console.log("\nPayment sent!")
   console.log("Amount sent: " + payment.getAmountSent())
   console.log("Amount delivered: " + payment.getAmountDelivered())
   console.log("Payment was " + (payment.getSuccessfulPayment() ? 'successful!' : 'unsuccessful!'))
