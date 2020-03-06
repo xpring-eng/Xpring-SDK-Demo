6e8a53ff3616f87459a14aaec41bb19ec007610c
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-06 13:15:39 -0500

    got build working

--- swift/ILP/Sources/main.swift
+++ swift/ILP/Sources/main.swift
@@ -6,11 +6,11 @@ let demoUserId = "demo_user"
 let demoUserAuthToken = "2S1PZh3fEKnKg"
 
 print("\nUsing Hermes node located at: \(grpcUrl) \n")
-let ilpClient = IlpClient(grpcUrl: grpcUrl)
+let ilpClient = IlpClient(grpcURL: grpcUrl)
 
 
 print("Retrieving balance for \(demoUserId)...")
-let getBalance = try ilpClient.getBalance(for: demoUserId, bearerToken: demoUserAuthToken)
+let getBalance = try ilpClient.getBalance(for: demoUserId, withAuthorization: demoUserAuthToken)
 print("Net balance was \(getBalance.netBalance) with asset scale \(getBalance.assetScale)")
 
 let receiverPaymentPointer = "$money.ilpv4.dev/demo_receiver"
@@ -19,15 +19,15 @@ print("\nSending payment:")
 print("- From: \(demoUserId)")
 print("- To: \(receiverPaymentPointer)")
 print("- Amount: \(amountToSend) drops")
-let payment = try ilpClient.sendPayment(amount: amountToSend,
-                                        paymentPointer: receiverPaymentPointer,
-                                        senderAccountId: demoUserId,
-                                        bearerToken: demoUserAuthToken)
+let payment = try ilpClient.sendPayment(amountToSend,
+                                        to: receiverPaymentPointer,
+                                        from: demoUserId,
+                                        withAuthorization: demoUserAuthToken)
 
 print("\nPayment sent!")
 print("Amount sent: \(payment.amountSent)")
 print("Amount delivered: \(payment.amountDelivered)")
 print("Payment was \((payment.successfulPayment ? "successful!" : "unsuccessful!"))")
 
-let balanceAfterPayment = try ilpClient.getBalance(for: demoUserId, bearerToken: demoUserAuthToken);
+let balanceAfterPayment = try ilpClient.getBalance(for: demoUserId, withAuthorization: demoUserAuthToken);
 print("Net balance after sending payment was \(balanceAfterPayment.netBalance)")
