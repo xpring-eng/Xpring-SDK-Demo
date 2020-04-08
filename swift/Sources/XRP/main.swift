470261a03c179e3f4b7ddaf9a9f3204ad4e1486a
Amie Corso <acorso@ripple.com>
2020-04-08 12:19:35 -0700

    add paymentHistory to XRPClient demo

--- swift/Sources/XRP/main.swift
+++ swift/Sources/XRP/main.swift
@@ -19,14 +19,17 @@ guard let wallet = Wallet(seed: seed) else {
 let recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4"
 let dropsToSend: UInt64 = 10
 
+// Instantiate an XRPClient connected to the XRP Ledger Testnet
 print("\nUsing rippled node located at: \(grpcAddress)\n")
 let xrpClient = XRPClient(grpcURL: grpcAddress, useNewProtocolBuffers: true)
 
+// Get account balance
 print("Retrieving balance for \(wallet.address) ..")
 let balance = try xrpClient.getBalance(for: wallet.address)
 
 print("Balance was \(balance) drops!\n")
 
+// Send XRP
 print("Sending:")
 print("- Drops \(dropsToSend)")
 print("- To: \(recipientAddress)")
@@ -35,5 +38,13 @@ let hash = try xrpClient.send(dropsToSend, to: recipientAddress, from: wallet)
 
 print("Hash for transaction:\n\(hash)\n")
 
+// Check status of the payment
 let status = try xrpClient.getTransactionStatus(for: hash)
 print("Result for transaction is:\n\(status)\n");
+
+// Retrieve full payment history for account
+print("Payment history for account " + wallet.address + ":\n")
+let paymentHistory = try xrpClient.paymentHistory(for: wallet.address)
+for payment in paymentHistory {
+  print(payment)
+}
