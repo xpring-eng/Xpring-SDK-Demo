3cdd49cc467f75d306a6b680966dc5323bec97ee
Amie <acorso@ripple.com>
2020-04-11 08:40:25 -0700

    Adds demo for `paymentHistory` method of `XRPClient` in Swift (#32)
    * pbxcodeproj
    
    * paymentDemos
    
    * Revert "pbxcodeproj"
    
    This reverts commit edf6344ba2592d470aa2f799546665988d294578.
    
    * pbxcodeproj
    
    * add paymentHistory to demos
    
    * limit paymentHistory output to max 5 transactions

--- swift/Sources/XRP/main.swift
+++ swift/Sources/XRP/main.swift
@@ -5,7 +5,7 @@ import XpringKit
 // Some options:
 //     dev.xrp.xpring.io:50051
 //     test.xrp.xpring.io:50051
-//     main.xrp.xpring.io:50051 
+//     main.xrp.xpring.io:50051
 let grpcAddress = "test.xrp.xpring.io:50051"
 
 // A wallet that exists on Testnet.
@@ -19,14 +19,17 @@ guard let wallet = Wallet(seed: seed) else {
 let recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4"
 let dropsToSend: UInt64 = 10
 
+// Instantiate an XRPClient connected to the XRP Ledger Testnet
 print("\nUsing rippled node located at: \(grpcAddress)\n")
 let xrpClient = XRPClient(grpcURL: grpcAddress, useNewProtocolBuffers: true)
 
+// Get account balance
 print("Retrieving balance for \(wallet.address) ..")
 let balance = try xrpClient.getBalance(for: wallet.address)
 
 print("Balance was \(balance) drops!\n")
 
+// Send XRP
 print("Sending:")
 print("- Drops \(dropsToSend)")
 print("- To: \(recipientAddress)")
@@ -35,5 +38,14 @@ let hash = try xrpClient.send(dropsToSend, to: recipientAddress, from: wallet)
 
 print("Hash for transaction:\n\(hash)\n")
 
+// Check status of the payment
 let status = try xrpClient.getTransactionStatus(for: hash)
 print("Result for transaction is:\n\(status)\n");
+
+// Retrieve full payment history for account
+print("Payment history for account " + wallet.address + ":\n")
+let paymentHistory = try xrpClient.paymentHistory(for: wallet.address)
+let shortPaymentHistory = paymentHistory.prefix(5) // limit long output
+for payment in shortPaymentHistory {
+  print(payment)
+}
