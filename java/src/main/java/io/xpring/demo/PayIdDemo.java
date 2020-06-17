fde06b39e408e9d30f7d1b1ecc85c564666dee35
Keefer Taylor <keefer.t@gmail.com>
2020-06-17 16:23:59 +0900

    offline todo

--- java/src/main/java/io/xpring/demo/PayIdDemo.java
+++ java/src/main/java/io/xpring/demo/PayIdDemo.java
@@ -5,6 +5,59 @@ import io.xpring.payid.idiomatic.PayIdException;
 public class PayIdDemo {
   public static void main(String[] args) throws PayIdException {
 
+const { PayIDClient, XRPPayIDClient, XRPLNetwork } = require("xpring-js")
+
+// The Pay ID to resolve.
+const payId = 'alice$dev.payid.xpring.money'
+
+// The XRP network to resolve on.
+const xrpNetwork = XRPLNetwork.Main
+
+// The BTC network to resolve on.
+const btcNetwork = 'btc-testnet'
+
+// A client to resolve PayIDs on the XRP Ledger.
+const xrpPayIdClient = new XRPPayIDClient(xrpNetwork);
+
+// A client to resolve PayIDs on the Bitcoin network.
+const btcPayIdClient = new PayIDClient(btcNetwork)
+
+    async function main() {
+      console.log("Resolving Pay ID: " + payId);
+      console.log("On network: " + networkToString(xrpNetwork))
+
+    const xrpAddress = await xrpPayIdClient.xrpAddressForPayID(payId)
+      console.log("Resolved to " + xrpAddress)
+      console.log("")
+
+      console.log("Resolving Pay ID: " + payId);
+      console.log("On network: " + btcNetwork)
+    const btcAddressComponents = await btcPayIdClient.addressForPayID(payId)
+      console.log("Resolved to " + btcAddressComponents.address)
+    }
+
+    function networkToString(network) {
+        switch (network) {
+          case XRPLNetwork.Dev:
+            return "Devnet"
+          case XRPLNetwork.Test:
+            return "Testnet"
+          case XRPLNetwork.Main:
+            return "Mainnet"
+          default:
+            return "Unknown Network"
+        }
+    }
+
+// Exit with an error code if there is an error.
+    process.on('unhandledRejection', error => {
+        console.log(`Fatal: ${error}`)
+        process.exit(1)
+    });
+
+
+    main()
+
 
 
     // TODO(keefertaylor): Implement.
