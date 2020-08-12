1ecca3faa2a5acaf6de816cce1b31aa7eee6e032
keefertaylor <keefer.t@gmail.com>
2020-08-12 11:50:07 -0700

    update swift

--- swift/Sources/PayID/main.swift
+++ swift/Sources/PayID/main.swift
@@ -41,3 +41,14 @@ case .success(let xrpAddress):
 case .failure(let error):
   fatalError("Unknown error resolving address: \(error)")
 }
+
+// Resolve all addresses
+print("Resolving All PayIDs for: \(payID)");
+guard case let .success(allAddresses) = payIDClient.allAddresses(for: payID) else {
+  fatalError("Could not resolve all addresses")
+}
+print("Resolved to:")
+for (index, address) in allAddresses.enumerated() {
+  print("\(index)) PaymentNetwork: \(address.paymentNetwork), Environment: \(String(describing: address.environment)), Address: \(address.addressDetails.address), Tag: \(String(describing: address.addressDetails.tag))")
+}
+print("")
