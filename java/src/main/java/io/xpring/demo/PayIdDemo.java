1b178a7ef7702d8b41f39d0ddba5ca0003ecd774
Keefer Taylor <keefer.t@gmail.com>
2020-08-18 11:37:13 -0700

    Add Functionality for Retrieving All of a PayID's Addresses (#47)
    * update node demos
    
    * update java
    
    * update swift

--- java/src/main/java/io/xpring/demo/PayIdDemo.java
+++ java/src/main/java/io/xpring/demo/PayIdDemo.java
@@ -1,6 +1,9 @@
 package io.xpring.demo;
 
+import java.util.List;
+
 import io.xpring.common.XrplNetwork;
+import io.xpring.payid.generated.model.Address;
 import io.xpring.payid.generated.model.CryptoAddressDetails;
 import io.xpring.payid.PayIdClient;
 import io.xpring.payid.XrpPayIdClient;
@@ -18,14 +21,14 @@ public class PayIdDemo {
     String btcNetwork = "btc-testnet";
 
     // A client to resolve PayIDs on any network..
-    PayIdClient btcPayIdClient = new PayIdClient();
+    PayIdClient payIdClient = new PayIdClient();
 
     // A client to resolve PayIDs on the XRP Ledger.
     XrpPayIdClient xrpPayIdClient = new XrpPayIdClient(xrpNetwork);
 
     System.out.println("Resolving PayID: " + payId);
     System.out.println("On network: " + btcNetwork);
-    CryptoAddressDetails btcAddressComponents = btcPayIdClient.cryptoAddressForPayId(payId, btcNetwork);
+    CryptoAddressDetails btcAddressComponents = payIdClient.cryptoAddressForPayId(payId, btcNetwork);
     System.out.println("Resolved to " + btcAddressComponents.getAddress());
     System.out.println("");
 
@@ -33,5 +36,13 @@ public class PayIdDemo {
     System.out.println("On network: " + xrpNetwork);
     String xrpAddress = xrpPayIdClient.xrpAddressForPayId(payId);
     System.out.println("Resolved to " + xrpAddress);
+    System.out.println("");
+
+    System.out.println("Resolving All PayIDs for: " + payId);
+    List<Address> allAddresses = payIdClient.allAddressesForPayId(payId);
+    System.out.println("Resolved to:");
+    for (int i = 0; i < allAddresses.size(); i++) {
+      System.out.println(i + ") " + allAddresses.get(i));
+    }
   }
 }
--- node/src/index-payid.js
+++ node/src/index-payid.js
@@ -1,51 +1,61 @@
-const { PayIdClient, XrpPayIdClient, XrplNetwork } = require("xpring-js")
+const { PayIdClient, XrpPayIdClient, XrplNetwork } = require("xpring-js");
 
 // The PayID to resolve.
-const payId = 'alice$dev.payid.xpring.money'
+const payId = "alice$dev.payid.xpring.money";
 
-// The XRP network to resolve on. 
-const xrpNetwork = XrplNetwork.Main
+// The XRP network to resolve on.
+const xrpNetwork = XrplNetwork.Main;
 
 // The BTC network to resolve on.
-const btcNetwork = 'btc-testnet'
+const btcNetwork = "btc-testnet";
 
 // A client to resolve PayIDs on the XRP Ledger.
 const xrpPayIdClient = new XrpPayIdClient(xrpNetwork);
 
 // A client to resolve PayIDs on the Bitcoin testnet.
-const btcPayIdClient = new PayIdClient()
+const payIdClient = new PayIdClient();
 
 async function main() {
-    console.log("Resolving PayID: " + payId);
-    console.log("On network: " + btcNetwork)
-    const btcAddressComponents = await btcPayIdClient.cryptoAddressForPayId(payId, btcNetwork)
-    console.log("Resolved to " + btcAddressComponents.address)
-    console.log("")
+  console.log("Resolving PayID: " + payId);
+  console.log("On network: " + btcNetwork);
+  const btcAddressComponents = await payIdClient.cryptoAddressForPayId(
+    payId,
+    btcNetwork
+  );
+  console.log("Resolved to " + btcAddressComponents.address);
+  console.log("");
 
-    console.log("Resolving PayID: " + payId);
-    console.log("On network: " + networkToString(xrpNetwork))
-    const xrpAddress = await xrpPayIdClient.xrpAddressForPayId(payId)
-    console.log("Resolved to " + xrpAddress)
+  console.log("Resolving PayID: " + payId);
+  console.log("On network: " + networkToString(xrpNetwork));
+  const xrpAddress = await xrpPayIdClient.xrpAddressForPayId(payId);
+  console.log("Resolved to " + xrpAddress);
+  console.log("");
+
+  console.log("Resolving All PayIDs for: " + payId);
+  const allAddresses = await payIdClient.allAddressesForPayId(payId);
+  console.log("Resolved to:");
+  for (let i = 0; i < allAddresses.length; i++) {
+    console.log(`${i}) ${JSON.stringify(allAddresses[i])}`);
+  }
 }
 
 function networkToString(network) {
-    switch (network) {
-        case XrplNetwork.Dev:
-            return "Devnet"
-        case XrplNetwork.Test:
-            return "Testnet"
-        case XrplNetwork.Main:
-            return "Mainnet"
-        default:
-            return "Unknown Network"
-    }
+  switch (network) {
+    case XrplNetwork.Dev:
+      return "Devnet";
+    case XrplNetwork.Test:
+      return "Testnet";
+    case XrplNetwork.Main:
+      return "Mainnet";
+    default:
+      return "Unknown Network";
+  }
 }
 
-// Exit with an error code if there is an error. 
-process.on('unhandledRejection', error => {
-    console.log(`Fatal: ${error}`)
-    process.exit(1)
-  });
-    
+// Exit with an error code if there is an error.
+process.on("unhandledRejection", (error) => {
+  console.log(`Fatal: ${error}`);
+  process.exit(1);
+});
 
-main()
+main();
