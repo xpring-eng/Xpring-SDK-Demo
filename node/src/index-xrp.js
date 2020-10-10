275fa876448a7c3270c0b486c9f2cc9bae590c24
Amie Corso <acorso@ripple.com>
2020-10-10 07:46:20 -0700

    figured out b58 seed string conversion back to mnemonic

--- node/src/index-xrp.js
+++ node/src/index-xrp.js
@@ -1,58 +1,49 @@
-const { Wallet, WalletFactory, XrplNetwork } = require("xpring-js")
+const { Wallet } = require("xpring-js")
 const bip39 = require('bip39')
 const addressCodec = require('ripple-address-codec')
+
 var mnemonic = 'seed sock milk update focus rotate barely fade car face mechanic mercy'
 
-// ORIGINAL
+// 32 char hex string = 128 bits https://github.com/bitcoinjs/bip39/blob/master/src/index.js
 var entropy = bip39.mnemonicToEntropy(mnemonic);
-console.log("entropy:")
+console.log("entropy length and entropy: " + entropy.length)
 console.log(entropy)
 
-var seedB58 = addressCodec.encodeSeed(Buffer.from(entropy, 'hex'), 'secp256k1');
-console.log("seedB58: ")
+// 16 byte array = 128 bits
+var entropyBuffer = Buffer.from(entropy, 'hex')
+console.log("entropy buffer length and buffer: " + entropyBuffer.length)
+console.log(entropyBuffer)
+
+// base 58 encoded string seed
+var seedB58 = addressCodec.encodeSeed(entropyBuffer, 'secp256k1');
+console.log("seed B58: ")
 console.log(seedB58)
 
+// 64 byte array - unclear what to do with this
 var seedBuffer = bip39.mnemonicToSeedSync(mnemonic)
+console.log("seedBuffer Length + seedBuffer vv: " + seedBuffer.length)
+console.log(seedBuffer)
+
+// object with several fields, including 16-byte array 'bytes' 
+// 'bytes' buffer is SAME AS ENTROPY BUFFER
+var decodedSeed = addressCodec.decodeSeed(seedB58)
+console.log("decodedSeed length + decodedSeed vv: " + decodedSeed.bytes.length)
+console.log(decodedSeed)
+
+// original word string
+var decodedMnemonic = bip39.entropyToMnemonic(decodedSeed.bytes)
+console.log("decodedMnemonic length + decodedMnemonic: " + decodedMnemonic.length)
+console.log(decodedMnemonic)
 
 var wallet = Wallet.generateWalletFromMnemonic(mnemonic, undefined, true);
 var wallet2 = Wallet.generateHDWalletFromSeed(seedBuffer, undefined, true);
-var wallet3 = Wallet.generateWalletFromSeed(seedB58)
-console.log("*** HDWALLET FROM MNEMONIC - orig")
+var wallet3 = Wallet.generateWalletFromMnemonic(decodedMnemonic, undefined, true)
+var wallet4 = Wallet.generateWalletFromSeed(seedB58)
+console.log("*** HDWALLET FROM MNEMONIC - original")
 console.log(JSON.stringify(wallet))
-console.log("*** HDWALLET FROM SEED Buffer - orig")
+console.log("*** HDWALLET FROM SEED BUFFER")
 console.log(JSON.stringify(wallet2))
-console.log("*** WALLET FROM B58 seed - orig")
+console.log("*** WALLET FROM DECODED MNEMONIC")
 console.log(JSON.stringify(wallet3))
-
-/**
-// USING WALLET
-var mnemonic = 'seed sock milk update focus rotate barely fade car face mechanic mercy'
-var seedBuffer = bip39.mnemonicToSeedSync(mnemonic)
-var wallet = Wallet.generateWalletFromMnemonic(mnemonic);
-var wallet2 = Wallet.generateHDWalletFromSeed(seedBuffer);
-console.log("*** WALLET FROM MNEMONIC -- Wallet")
-console.log(JSON.stringify(wallet))
-console.log("*** WALLET FROM SEED -- Wallet")
-console.log(JSON.stringify(wallet2))
-
-
-// USING WALLET FACTORY
-async function testWalletFactory() {
-  var mnemonic =
-  'seed sock milk update focus rotate barely fade car face mechanic mercy'
-  var walletFactory = new WalletFactory(XrplNetwork.Test)
-  var seedBuffer = bip39.mnemonicToSeedSync(mnemonic)
-  var wallet = await walletFactory.walletFromMnemonicAndDerivationPath(
-  mnemonic,
-  )
-  var wallet2 = walletFactory.walletFromSeedAndDerivationPath(
-  seedBuffer.toString('hex'),
-  )
-  console.log('*** WALLET FROM MNEMONIC -- WalletFactory')
-  console.log(JSON.stringify(wallet))
-  console.log('*** WALLET FROM SEED -- WalletFactory')
-  console.log(JSON.stringify(wallet2))
-}
-
-testWalletFactory().then(()=> {})
-*/
+console.log("***WALLET FROM SEED - not HD")
+console.log(JSON.stringify(wallet4))
